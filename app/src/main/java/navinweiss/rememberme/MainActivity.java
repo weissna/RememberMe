dd84c20bdffde61552a4c4a0011c97090a3a7d78
weissna <weissna@rose-hulman.edu>
2016-08-01 13:08:28 -0400

    Got the phone thing working. Ish. Still don't know how to get the number from strings

--- app/src/main/java/navinweiss/rememberme/MainActivity.java
+++ app/src/main/java/navinweiss/rememberme/MainActivity.java
@@ -1,13 +1,10 @@
 package navinweiss.rememberme;
 
-import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,33 +18,41 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.emergency_button);
-
+        final Context c = this;
         // add button listener
         button.setOnClickListener(new View.OnClickListener() {
+
 
             @SuppressWarnings("MissingPermission")
             @Override
             public void onClick(View view) {
-
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse(String.valueOf(R.string.emergency_number)));
-                if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    Log.d("ttt", "Something with permissions");
-                    return;
-                }
-                startActivity(callIntent);
+                doTheDo();
+////                Intent callIntent = new Intent(MainActivity.this, Intent.ACTION_CALL);
+//                Intent callIntent = new Intent(Intent.ACTION_CALL);
+//                callIntent.setData(Uri.parse(String.valueOf(R.string.emergency_number)));
+//                if (ActivityCompat.checkSelfPermission(c, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//                    // TODO: Consider calling
+//                    //    ActivityCompat#requestPermissions
+//                    // here to request the missing permissions, and then overriding
+//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                    //                                          int[] grantResults)
+//                    // to handle the case where the user grants the permission. See the documentation
+//                    // for ActivityCompat#requestPermissions for more details.
+//                    Log.d("ttt", "Something with permissions");
+//                    return;
+//                }
+//                startActivity(callIntent);
 
             }
 
         });
 
+    }
+
+    private void doTheDo() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:8122295424"));
+        startActivity(intent);
     }
 
 }
