1445d150355a5f945a090e9a189f556c28a68389
weissna <weissna@rose-hulman.edu>
2016-08-04 12:49:25 -0400

    Nothing

--- app/src/main/java/navinweiss/rememberme/MainActivity.java
+++ app/src/main/java/navinweiss/rememberme/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity {
@@ -20,8 +19,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference ref = db.getReference("notes");
-        ref.setValue("Yo, world1");
+//        DatabaseReference ref = db.getReference("notes");
+//        ref.setValue("Yo, world1");
 
         emergencyCallButton = (Button) findViewById(R.id.emergency_button);
         notesButton = (Button) findViewById(R.id.ReminderButton);
