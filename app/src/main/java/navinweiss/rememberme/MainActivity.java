5b293cd8d748b617213172b388360ca3b69b8303
weissna <weissna@rose-hulman.edu>
2016-08-18 11:25:00 -0400

    I don't commit very often, do I? This is probably my last commit unless I decide to keep this going, which I might. Core functionality working, although it doesn't look very nice, and geotracking isn't working

--- app/src/main/java/navinweiss/rememberme/MainActivity.java
+++ app/src/main/java/navinweiss/rememberme/MainActivity.java
@@ -1,5 +1,6 @@
 package navinweiss.rememberme;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
@@ -20,15 +21,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, NoteAdapter.Callback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private Button emergencyCallButton;
     private Button notesButton;
@@ -38,22 +43,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public FirebaseDatabase db;
     private GoogleMap mMap;
     private List<LatLng> routePoints = new ArrayList<>();
+    private DatabaseReference mDBRefNotes;
     private DatabaseReference mDBRef;
-    private NoteAdapter mAdapter;
+    private String phNo;
+//    private NoteAdapter mAdapter;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAdapter = new navinweiss.rememberme.NoteAdapter(this);
+//        mAdapter = new navinweiss.rememberme.NoteAdapter(this);
         db = setupDatabase();
         readFromDatabase(db);
-        mDBRef = FirebaseDatabase.getInstance().getReference().child("notes");
+        mDBRefNotes = FirebaseDatabase.getInstance().getReference().child("notes");
+        mDBRef = FirebaseDatabase.getInstance().getReference();
+        phNo = "8122295424";
 
         newNoteButton = (Button) findViewById(R.id.newNoteButton);
         emergencyCallButton = (Button) findViewById(R.id.emergency_button);
         notesButton = (Button) findViewById(R.id.ReminderButton);
         saveNoteButton = (Button) findViewById(R.id.saveNoteButton);
         newNoteText = (TextView) findViewById(R.id.noteText);
+        newNoteText.setVisibility(View.INVISIBLE);
 
         // add button listener
         setButtonListeners();
@@ -66,38 +76,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    public void locationSheniganry(){
+    public void locationSheniganry() {
         LocationManager service = (LocationManager) getSystemService(this.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
         String provider = service.getBestProvider(criteria, false);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+//            @Override
+//            public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+//
+//            }
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Location location = service.getLastKnownLocation(provider);
-        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
-        routePoints.add(userLocation);
+//        Location location = service.getLastKnownLocation(provider);
+//        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+//        routePoints.add(userLocation);
 
         service.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                LatLng temp = new LatLng(location.getLatitude(),location.getLongitude());
+                LatLng temp = new LatLng(location.getLatitude(), location.getLongitude());
                 routePoints.add(temp);
+                mMap.addMarker(new MarkerOptions().position(temp).title(("" + Calendar.getInstance().getTime())));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(temp, 17.0f));
             }
+
             @Override
             public void onProviderDisabled(String provider) {
                 // TODO Auto-generated method stub
             }
+
             @Override
             public void onProviderEnabled(String provider) {
                 // TODO Auto-generated method stub
             }
+
             @Override
             public void onStatusChanged(String provider, int status,
                                         Bundle extras) {
@@ -114,41 +131,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         LocationManager locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
 
-
-        // Add a marker in Sydney and move the camera
         LatLng terreHaute = new LatLng(39.4696, -87.3898);
-        final Marker terreHauteMarker = mMap.addMarker(new MarkerOptions().position(terreHaute).title("Terre Haute"));
+        mMap.addMarker(new MarkerOptions().position(terreHaute));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(terreHaute, 17.0f));
         routePoints.add(terreHaute);
         locationSheniganry();
 
-//        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-//            @Override
-//            public void onMapLongClick(LatLng latLng) {
-//                createMarker(latLng);
-//            }
-//        });
-//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-//
-//            @Override
-//            public boolean onMarkerClick(Marker marker) {
-//                Toast.makeText(MainActivity.this, "You clicked a marker", Toast.LENGTH_SHORT).show();
-//                if (marker.equals(terreHauteMarker)) {
-//                    Toast.makeText(MainActivity.this, "You clicked Terre Haute", Toast.LENGTH_SHORT).show();
-//                }
-//                return false;
-//            }
-//        });
     }
 
 
-
-    private void doTheDo() {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        String temp = this.getString(R.string.emergency_number);
-        intent.setData(Uri.parse("tel:"+temp));
-        startActivity(intent);
-    }
 
     private void readFromDatabase(FirebaseDatabase db) {
 //        Log.d("ttt", "Read stuff");
@@ -161,8 +152,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FirebaseDatabase setupDatabase() {
         db = FirebaseDatabase.getInstance();
         DatabaseReference ref = db.getReference("notes");
-        Note temp = new Note("Temp note");
-        ref.push().child("note").setValue(temp.getNote());
+//        Note temp = new Note("Temp note");
+//        ref.push().child("note").setValue(temp.getNote());
+
         return db;
     }
 
@@ -173,9 +165,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
                 Log.d("ttt", "emergency?");
-                doTheDo();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                mAdapter.
+//                final String[] s = {""};
+                Log.d("phon", getPhoneNumber());
+                intent.setData(Uri.parse("tel:"+getPhoneNumber()));
+                startActivity(intent);
             }
         });
+
+//        emergencyCallButton.setOnLongClickListener(new View.OnLongClickListener(){
+//
+//            @Override
+//            public boolean onLongClick(View v) {
+//            }
+//        });
 
         notesButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -192,9 +196,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             @Override
             public void onClick(View v) {
+                newNoteButton.setVisibility(View.INVISIBLE);
                 saveNoteButton.setVisibility(View.VISIBLE);
                 newNoteText.setVisibility(View.VISIBLE);
                 newNoteText.setFocusable(true);
+                newNoteText.requestFocus();
             }
         });
 
@@ -206,22 +212,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //                    Note temp = new Note(newNoteText.getText().toString());
 //                    mAdapter.add(temp);
 //                }
+                mDBRefNotes.push().child("note").setValue(newNoteText.getText().toString());
+                newNoteText.setText("");
+                newNoteText.setVisibility(View.INVISIBLE);
+                saveNoteButton.setVisibility(View.INVISIBLE);
+                newNoteButton.setVisibility(View.VISIBLE);
             }
         });
+    }
 
-        newNoteText.setOnClickListener(new View.OnClickListener(){
+    private String getPhoneNumber() {
+        mDBRef.addValueEventListener(new ValueEventListener() {
 
             @Override
-            public void onClick(View v) {
-                newNoteText.setFocusable(true);
-                newNoteText.requestFocus();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Iterable<DataSnapshot> t = dataSnapshot.child("phone").child("emergCon").getChildren();
+                for(DataSnapshot d : t) {
+                    phNo = d.getValue().toString();
+                    Log.d("phoe", phNo);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+        return phNo;
     }
 
 
-    @Override
-    public void onEdit(Note note) {
+    class NotesChildEventListener implements ChildEventListener{
 
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            Note note = dataSnapshot.getValue(Note.class);
+            note.setKey(dataSnapshot.getKey());
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
     }
+
+
 }
--- app/src/main/java/navinweiss/rememberme/NotesActivity.java
+++ app/src/main/java/navinweiss/rememberme/NotesActivity.java
@@ -3,10 +3,9 @@ package navinweiss.rememberme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -36,91 +35,27 @@ public class NotesActivity extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notesandreminders);
-        saveNote = (Button) findViewById(R.id.save_note);
-        saveNote.setVisibility(View.INVISIBLE);
-        newNoteButton = (Button) findViewById(R.id.newNoteButton);
+        last = (TextView) findViewById(R.id.note_text);
+
         readNotesFromDatabase();
-
-
-//        note = (EditText) findViewById(R.id.note1);
-
-//        final RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_View);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setHasFixedSize(true);
-
-//        note.setOnTouchListener(new View.OnTouchListener(){
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                newNoteButton.setVisibility(View.INVISIBLE);
-//                saveNote.setVisibility(View.VISIBLE);
-//                return false;
-//            }
-//        });
-
-        newNoteButton.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                RelativeLayout mRlayout = (RelativeLayout) findViewById(R.id.relLayout);
-                RelativeLayout.LayoutParams mRparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                note = new EditText(NotesActivity.this);
-                note.setLayoutParams(mRparams);
-                mRlayout.addView(note);
-                note.setVisibility(View.VISIBLE);
-                note.setHint("Write your note here");
-                saveNote.setVisibility(View.VISIBLE);
-            }
-        });
-
-        saveNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                writeNoteToDatabase(note != null? note.getText().toString() : "");
-                RelativeLayout mRlayout = (RelativeLayout) findViewById(R.id.relLayout);
-                RelativeLayout.LayoutParams mRparams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
-                last = (TextView) findViewById(R.id.textView);
-                TextView temp1 = new TextView(NotesActivity.this);
-                mRparams.addRule(RelativeLayout.BELOW, last.getExtendedPaddingBottom());
-                mRlayout.addView(temp1);
-
-                temp1.setText(note.getText().toString());
-                temp1.setFocusable(false);
-                last = temp1;
-
-
-//                mNote = new Note(recyclerView);
-//                recyclerView.setAdapter(mNote);
-                if(note != null) {
-                    String temp = note.getText().toString();
-                    mNotes.add(temp);
-                    numOfNotes++;
-                }
-//                Note newNote = new Note(temp);
-
-//                readNotesFromDatabase();
-            }
-        });
-        saveNote.setVisibility(View.VISIBLE);
     }
 
-    public void writeNoteToDatabase(String str){
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference ref = db.getReference("notes");
-
-
-        String refStr = "notes" + numOfNotes;
-        ref.child(refStr).setValue(str);
-    }
 
     public void readNotesFromDatabase(){
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference ref = db.getReference("notes");
-        ref.child("notes").addListenerForSingleValueEvent(new ValueEventListener() {
+        final DatabaseReference ref = db.getReference("notes");
+
+
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String s = dataSnapshot.getValue(String.class);
-                Log.d("ttt", "String: "+s);
+                Iterable<DataSnapshot> s = dataSnapshot.getChildren();
+                for(DataSnapshot d : s){
+                    String t = d.child("note").getValue().toString();
+                    mNotes.add(t);
+                    Log.d("sss", t);
+                }
+                displayNotes(mNotes);
             }
 
             @Override
@@ -128,24 +63,19 @@ public class NotesActivity extends Activity
 
             }
         });
-//        ValueEventListener v = new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                String str = dataSnapshot.getValue(String.class);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        };
-//        ref.addValueEventListener(v);
 
-//        String temp = ref.child().
-//        Log.d("ttt", "Made it this far: "+temp);
-////        int i = Integer.parseInt(temp);
-////        Log.d("ttt", "Num: "+i);
+    }
 
+    private void displayNotes(List<String> mNotes) {
+        LinearLayout linearLayout = new LinearLayout(this);
+        setContentView(linearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        for(int i = 0; i<mNotes.size();i++){
+            TextView v = new TextView(this);
+            v.setText(mNotes.get(i));
+            v.setTextSize(15);
+            linearLayout.addView(v);
+        }
     }
 
 
